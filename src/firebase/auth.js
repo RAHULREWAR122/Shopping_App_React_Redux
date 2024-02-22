0e8816c41984bebe43bfaf8079b1078d8bf613d1
RAHUL REWAR <133180470+RAHULREWAR122@users.noreply.github.com>
2024-02-22 09:26:09 +0530

    Update auth.js

--- src/firebase/auth.js
+++ src/firebase/auth.js
@@ -4,12 +4,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyABg_Ld9zIm7YGajEveItt0McfTaZO8J84",
-  authDomain: "shopping-app-c8122.firebaseapp.com",
-  projectId: "shopping-app-c8122",
-  storageBucket: "shopping-app-c8122.appspot.com",
-  messagingSenderId: "839385534663",
-  appId: "1:839385534663:web:40b6975fba9a4c30d12ee2",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 };
 const app = initializeApp(firebaseConfig);
 const auth = getAuth();
